1836a3b767768c962f721ab65e290642a13cc708
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2021-10-13 14:51:22 +0900

    테스트

---
+++ calc-clock/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Calc from "./Calculator/Calc";
+function App() {
+  return (
+    <div className="App">
+      <Calc></Calc>
+    </div>
+  );
+}
+
+export default App;
